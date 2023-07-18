29e9b133dad6c8a6f51395abe8bc66ac6dba69b4
RashuPavan <hulsurkar4@gmail.com>
2023-07-18 12:30:00 +0530

    mycommit

---
+++ src/com/app/SecondHighest.java
@@ -0,0 +1,18 @@
+package com.app;
+
+public class SecondHighest {
+
+	public static void main(String args[])
+	{
+		int arr[]= {100,14,46,47,94,94,52,86,36,94,89};
+		int max=arr[0];
+	
+		for(int i=0;i<arr.length-1;i++)
+		{
+			if(arr[i+1]>max)
+				max=arr[i];
+		}
+		
+		System.out.println(max);
+	}
+}
